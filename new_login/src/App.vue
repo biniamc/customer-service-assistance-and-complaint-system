b451f3eafa263027ad1a77807589f4a0804eaa02
biniamc <biniamcoding@gmail.com>
2021-04-24 00:57:54 -0700

    add existing file

---
+++ new_login/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Login />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login,
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ new_login/src/main.js
@@ -0,0 +1,11 @@
+//import './plugins/bootstrap-vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// eslint-disable-next-line no-undef
+
+createApp(App).mount('#app')
